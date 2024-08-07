ec49072f440719676324e37317a81ba90e9196fc
Arshad <121594418+Md-Arshad22@users.noreply.github.com>
2024-08-07 16:24:01 +0530

    Update handling.java

--- handling.java
+++ handling.java
@@ -2,7 +2,7 @@ public class Handling{
   public static void main(String[] args) throws IOException{
     file myfile = new File("Arshad.txt");
     myfile = createnewfile();
-    
+    System.out.println(myfile.exists());
   }
 
 }
